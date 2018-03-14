caf1a667e0d6173cf0eecbb987dbd757203abf5d
Daniel Hansen <dph1108@gmail.com>
2018-03-14 12:10:39 +0100

    Added some names and occupations to the start of the dept bage

--- Aflevering 2/DebtBook/DebtBook/MainWindow.xaml.cs
+++ Aflevering 2/DebtBook/DebtBook/MainWindow.xaml.cs
@@ -23,6 +23,23 @@ namespace DebtBook
         public MainWindow()
         {
             InitializeComponent();
+            List<Person> users = new List<Person>();
+            users.Add(new Person() { Id = 1, Name = "John Doe", Surname = "Martino", Debt = 50, Occupation = "Martina" });
+            users.Add(new Person() { Id = 2, Name = "Jane Doe", Surname = "Martina", Debt = 50, Occupation = "Herpina"});
+            users.Add(new Person() { Id = 3, Name = "Sammy Doe", Surname = "Fatima", Debt = 50, Occupation = "Hehe"});
+
+            Debitors.ItemsSource = users;
+
         }
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public double Debt { get; set; }
+            public string Occupation { get; set; }
+        }
+
     }
+
 }
