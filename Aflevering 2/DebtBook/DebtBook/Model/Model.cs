86b857d4ee9bbadb63be32fc1152f0d89548922e
FatimaAU <fatima_kodro@hotmail.com>
2018-03-15 16:03:12 +0100

    Modified some stuff

--- Aflevering 2/DebtBook/DebtBook/Model/Model.cs
+++ Aflevering 2/DebtBook/DebtBook/Model/Model.cs
@@ -4,33 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DebtModel
+namespace DebtBook.Model
 {
     class Model
     {
-        
-    }
-
-    class Debitor
-    {
         public string Name { get; set; }
-
-        List<Debits> Debts = new List<Debits>();
-
-        public void setDebt(string title ,double amount)
-        {
-            Debits setDeb = new Debits();
-            setDeb.Amount = amount;
-            setDeb.Title = title;
-            Debts.Add(setDeb);
-            Debts.Add(setDeb);
-        }
+        public double Debt { get; set; }
     }
 
-    class Debits
-    {
-        public string Title { get; set; }
-        public double Amount { get; set; }
-    }
+    //class Debitor
+    //{
+    //    public string Name { get; set; }
+
+    //    List<Debits> Debts = new List<Debits>();
+
+    //    public void setDebt(string title ,double amount)
+    //    {
+    //        Debits setDeb = new Debits();
+    //        setDeb.Amount = amount;
+    //        setDeb.Title = title;
+    //        Debts.Add(setDeb);
+    //        Debts.Add(setDeb);
+    //    }
+    //}
+
+    //class Debits
+    //{
+    //    public string Title { get; set; }
+    //    public double Amount { get; set; }
+    //}
 
 }
