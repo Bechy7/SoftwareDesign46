69448001869c77912959df9c2d7729754d5eb0db
DPH <DPH1108@gmail.com>
2018-03-16 16:23:11 +0100

    Changed Overview and OverviewModel with working Text

--- Aflevering 2/DebtBook/DebtBook/ViewModels/OverViewModel.cs
+++ Aflevering 2/DebtBook/DebtBook/ViewModels/OverViewModel.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Navigation;
+using Prism.Mvvm;
 using DebtBook.Model;
-
 namespace DebtBook.ViewModels
 {
-    public class OverViewModel
+    public class OverViewModel : BindableBase
     {
-        
+        private string _text = "Hans";
+
+        public string Text
+        {
+            get { return _text; }
+            set { SetProperty(ref _text, value); }
+        }
+
+        public OverViewModel()
+        {
+            //Text = "Hans";    
+            //debts_.Add(new DebitorModel() { id = 1, name = "Hans" , debting = 50 });
+        }
     }
 }
