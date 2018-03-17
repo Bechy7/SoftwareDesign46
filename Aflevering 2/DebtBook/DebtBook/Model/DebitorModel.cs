0ea79b6dd2e17974ec7d45603b4bf1b462e83d4c
DPH <DPH1108@gmail.com>
2018-03-17 12:17:16 +0100

    Tilføjet kommentarer og ryddet koden op så den i det mindste ikke ligner helt lort.

--- Aflevering 2/DebtBook/DebtBook/Model/DebitorModel.cs
+++ Aflevering 2/DebtBook/DebtBook/Model/DebitorModel.cs
@@ -13,49 +13,7 @@ namespace DebtBook.Model
         //public int id { get; set; }
     }
 
-    //Da person
-    public class Debitor
-    {
-        //Konstruktør til at gøre det nemmere at tilføje debitors
-        Debitor(int id, string name, Debt debts)
-        {
-            Id = id;
-            Name = name;
-            //Hvis der ikke er tilføjet nogen liste (den første ID) så lav en ny liste klar
-            if(Debts == null)
-            {
-                Debts = new List<Debt>();
-            }
-            //Add whatever der er fra debts ind
-            Debts.Add(debts);
-        }
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Debt> Debts { get; set; }
-    }
-    //Da munnies
-    public class Debt
-    {
-        //Super weirdo ass constructor
-        public Debt(int id, string subject, double value, Debt debts)
-        {
-            debts.Value = value;
-            debts.Subject = subject;
-            debts.Id = id;
-        }
-        //Simplified
-        public Debt()
-        {
-        }
-
-        public int Id { get; set; }
-        public string Subject { get; set; }
-        public double Value { get; set; }
-        public List<Debitor> Debitors { get; set; }
-    }
-
-    //Trial and error ting SIMPLIFY
+    //Trial and error ting SIMPLIFY, den her virker med den nuværende version
     public class Person
     {
         public int Id { get; set; }
@@ -63,4 +21,46 @@ namespace DebtBook.Model
         public double Debt { get; set; }
     }
     
+    ////Andre versioner. Logikken er abnormal kompliceret og KISS er en del af det her shite
+    ////Da person
+    //public class Debitor
+    //{
+    //    //Konstruktør til at gøre det nemmere at tilføje debitors
+    //    Debitor(int id, string name, Debt debts)
+    //    {
+    //        Id = id;
+    //        Name = name;
+    //        //Hvis der ikke er tilføjet nogen liste (den første ID) så lav en ny liste klar
+    //        if (Debts == null)
+    //        {
+    //            Debts = new List<Debt>();
+    //        }
+    //        //Add whatever der er fra debts ind
+    //        Debts.Add(debts);
+    //    }
+
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public List<Debt> Debts { get; set; }
+    //}
+    ////Da munnies
+    //public class Debt
+    //{
+    //    //Super weirdo ass constructor
+    //    public Debt(int id, string subject, double value, Debt debts)
+    //    {
+    //        debts.Value = value;
+    //        debts.Subject = subject;
+    //        debts.Id = id;
+    //    }
+    //    //Simplified
+    //    public Debt()
+    //    {
+    //    }
+
+    //    public int Id { get; set; }
+    //    public string Subject { get; set; }
+    //    public double Value { get; set; }
+    //    public List<Debitor> Debitors { get; set; }
+    //}
 }
--- Aflevering 2/DebtBook/DebtBook/ViewModels/OverViewModel.cs
+++ Aflevering 2/DebtBook/DebtBook/ViewModels/OverViewModel.cs
@@ -9,65 +9,69 @@ using System.Windows.Navigation;
 using Prism.Mvvm;
 using DebtBook.Model;
 
-
 namespace DebtBook.ViewModels
 {
-    //First try
+    //BindableBase giver os Prism.Mvvm med observablecollection, Observablecollection giver os automatisk events som Inotify og changes
     public class OverViewModel : BindableBase
     {
-        private string _text = "Hans";
-        Person obj1;
+        //private string _text = "Hans"; //MIS (Må ikke slettes) Er crucial for koden til at virke
 
+
+        //Lav en observableliste af den liste du har i Model (I det her tilfælde <Person>)
+        ObservableCollection<Person> debtlist;
         //Navnet ligger i XAML filen, jeg er sikker på dette navn (pga. bindings sucks) og derfor har jeg "afbenyttet" mig af dette navn, pls don't delete or change indtil du er sikker at du kan finde den korrekte binding
-        public ObservableCollection<Person> Text
+        public ObservableCollection<Person> DebtList
         {
             get
             {
                 if (debtlist == null)
                 {
                     debtlist = new ObservableCollection<Person>();
-                    
                 }
                 return debtlist;
             }
-            //get { return debtlist.ElementAt(1); }
             set { SetProperty(ref debtlist, value); }
-
         }
 
+        //Konstruktøreren er det eneste der bliver eksekveret i Overview Xaml, tilføj evt. klasserne her
         public OverViewModel()
         {
             //Laver en ny liste fra Persons elementerne
             debtlist = new ObservableCollection<Person>();
+            //Husk at tilføje debts hardcoded ind
+            AddDebt();
+        }
+ 
+        //Hardcoded Adder
+        public void AddDebt()
+        {
+            //Adder en masse hardcoded stuff
+            //debtlist = new ObservableCollection<Person>(debtlist); //Har man måske ikke brug for
             debtlist.Add(new Person() { Id = 1, Name = "Hans", Debt = 500.1373 });
             debtlist.Add(new Person() { Id = 2, Name = "Get", Debt = 300.7331 });
             debtlist.Add(new Person() { Id = 3, Name = "Ze", Debt = 999.1337 });
-            debtlist.Add(new Person() { Id = 3, Name = "FLAMMENWERFER", Debt = 9191.1337 });
-            debtlist.Add(new Person() { Id = 3, Name = "SOFORT", Debt = 1337 });
-        }
-        ObservableCollection<Person> debtlist;
-        public ObservableCollection<Person> Debtlist
-        {
-            get
-            {
-                //Lav en ny liste hvis der er nul
-                if (Debtlist == null)
-                {
-                    Debtlist = new ObservableCollection<Person>();
-                }
-                return debtlist;
-
-            }
-            set { debtlist = value; RaisePropertyChanged(); }
-        }
-        public void addDebt()
-        {
-            debtlist = new ObservableCollection<Person>(debtlist);
-            debtlist.Add(new Person() { Id = 1, Name = "Hans", Debt = 500 });
-            debtlist.Add(new Person() { Id = 2, Name = "Hans", Debt = 500 });
+            debtlist.Add(new Person() { Id = 4, Name = "FLAMMENWERFER", Debt = 9191.1337 });
+            debtlist.Add(new Person() { Id = 5, Name = "SOFORT", Debt = 1337 });
+            debtlist.Add(new Person() { Id = 6, Name = "Hans", Debt = 500 });
+            debtlist.Add(new Person() { Id = 7, Name = "Hans", Debt = 500 });
         }
     }
 
+
+    //public ObservableCollection<Person> Debtlist
+    //{
+    //    get
+    //    {
+    //        //Lav en ny liste hvis der er nul
+    //        if (Debtlist == null)
+    //        {
+    //            Debtlist = new ObservableCollection<Person>();
+    //        }
+    //        return debtlist;
+    //
+    //    }
+    //    set { debtlist = value; RaisePropertyChanged(); }
+    //}
     //public Debitor SelectedDebitor
     //{
     //    get
@@ -96,87 +100,87 @@ namespace DebtBook.ViewModels
 
 
 }
-    //Second try
-    //public class DebitorVM : BindableBase
-    //{
-    //    Debitor Debitors;
-    //    public DebitorVM(Debitor person)
-    //    {
-    //        Debitors = person;
-    //    }
+//Second try
+//public class DebitorVM : BindableBase
+//{
+//    Debitor Debitors;
+//    public DebitorVM(Debitor person)
+//    {
+//        Debitors = person;
+//    }
 
-    //    public int Id
-    //    {
-    //        get
-    //        {
-    //            return Debitors.Id;
-    //        }
+//    public int Id
+//    {
+//        get
+//        {
+//            return Debitors.Id;
+//        }
 
-    //        //Tester nogle forskellige "Changes"
-    //        set { Debitors.Id = value; RaisePropertyChanged(); }
-            
-    //    }
-    //    public string Name
-    //    {
-    //        get
-    //        {
-    //            //string name = 
-    //            return Debitors.Name = "HansSS";
-    //        }
+//        //Tester nogle forskellige "Changes"
+//        set { Debitors.Id = value; RaisePropertyChanged(); }
 
-    //        //Tester nogle forskellige "Changes"
-    //        set { Debitors.Name = value; RaisePropertyChanged(); }
-    //    }
-    //    //Gør at lortet bliver instantieret og man kan kun ændre listen herfra
-    //    ObservableCollection<Debt> debtlist;
-    //    public ObservableCollection<Debt> Debtlist
-    //    {
-    //        get
-    //        {
-    //            //Lav en ny liste hvis der er nul
-    //            if (Debtlist == null)
-    //            {
-    //                Debtlist = new ObservableCollection<Debt>(Debitors.Debts);
-    //            }
-    //            return debtlist;
+//    }
+//    public string Name
+//    {
+//        get
+//        {
+//            //string name = 
+//            return Debitors.Name = "HansSS";
+//        }
 
-    //        }
-    //        set { debtlist = value; RaisePropertyChanged(); }
-    //    }
-    //    //Adder stuff til listen i model så man kan se dem i VIEW
-    //    public void AddDebitor()
-    //    {
+//        //Tester nogle forskellige "Changes"
+//        set { Debitors.Name = value; RaisePropertyChanged(); }
+//    }
+//    //Gør at lortet bliver instantieret og man kan kun ændre listen herfra
+//    ObservableCollection<Debt> debtlist;
+//    public ObservableCollection<Debt> Debtlist
+//    {
+//        get
+//        {
+//            //Lav en ny liste hvis der er nul
+//            if (Debtlist == null)
+//            {
+//                Debtlist = new ObservableCollection<Debt>(Debitors.Debts);
+//            }
+//            return debtlist;
 
-    //        Debitors.Debts.Add(new Debt() { Id = 1, Value = 20, Subject = "Flammenwerfer" });
-            
-    //    }
+//        }
+//        set { debtlist = value; RaisePropertyChanged(); }
+//    }
+//    //Adder stuff til listen i model så man kan se dem i VIEW
+//    public void AddDebitor()
+//    {
 
-    //}
-    //Trial and error ting Adder som heller ikke virker
-    //public class ListofPeople : BindableBase
-    //{
+//        Debitors.Debts.Add(new Debt() { Id = 1, Value = 20, Subject = "Flammenwerfer" });
 
-    //    ObservableCollection<Person> debtlist;
-    //    public ObservableCollection<Person> Debtlist
-    //    {
-    //        get
-    //        {
-    //            //Lav en ny liste hvis der er nul
-    //            if (Debtlist == null)
-    //            {
-    //                Debtlist = new ObservableCollection<Person>(debtlist);
-    //            }
-    //            return debtlist;
+//    }
 
-    //        }
-    //        set { debtlist = value; RaisePropertyChanged(); }
-    //    }
-    //    public void addDebt()
-    //    {
-    //        debtlist = new ObservableCollection<Person>(debtlist);
-    //        debtlist.Add(new Person() { Id = 1, Name = "Hans", Debt = 500 });
-    //        debtlist.Add(new Person() { Id = 2, Name = "Hans", Debt = 500 });
-    //    }
-    //}
+//}
+//Trial and error ting Adder som heller ikke virker
+//public class ListofPeople : BindableBase
+//{
+
+//    ObservableCollection<Person> debtlist;
+//    public ObservableCollection<Person> Debtlist
+//    {
+//        get
+//        {
+//            //Lav en ny liste hvis der er nul
+//            if (Debtlist == null)
+//            {
+//                Debtlist = new ObservableCollection<Person>(debtlist);
+//            }
+//            return debtlist;
+
+//        }
+//        set { debtlist = value; RaisePropertyChanged(); }
+//    }
+//    public void addDebt()
+//    {
+//        debtlist = new ObservableCollection<Person>(debtlist);
+//        debtlist.Add(new Person() { Id = 1, Name = "Hans", Debt = 500 });
+//        debtlist.Add(new Person() { Id = 2, Name = "Hans", Debt = 500 });
+//    }
+//}
 
 
