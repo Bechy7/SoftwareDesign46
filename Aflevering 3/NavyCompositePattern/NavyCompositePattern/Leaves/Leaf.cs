c54a18e1457f866f01871a0681b297aa29e0e6df
Daniel Hansen <dph1108@gmail.com>
2018-04-11 12:04:48 +0200

    Leaf

---
+++ Aflevering 3/NavyCompositePattern/NavyCompositePattern/Leaves/Leaf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NavyCompositePattern.Interfaces;
+
+namespace NavyCompositePattern.Leaves
+{
+    public class Leaf : Component
+    {
+        void Operation()
+        {
+            Console.WriteLine("Leaf");
+        }
+    }
+}
