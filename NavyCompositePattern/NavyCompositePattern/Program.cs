41701c77983f69a4bbcab91733a60ca3af86b932
Daniel Hansen <dph1108@gmail.com>
2018-04-11 11:19:29 +0200

    Tilføjet en person mere til staff

--- NavyCompositePattern/NavyCompositePattern/Program.cs
+++ NavyCompositePattern/NavyCompositePattern/Program.cs
@@ -14,15 +14,17 @@ namespace NavyCompositePattern
         static void Main(string[] args)
         {
             Staff _staff = new Staff();
-            IPrivate _pfc = new IPrivate {ID = 0,Name = "Fatima",Weapon ="Dildogun"};
+            IPrivate _pfc1 = new IPrivate {ID = 0,Name = "Fatima",Weapon ="M4A1"};
+            IPrivate _pfc2 = new IPrivate{ID = 1, Name = "Daniel", Weapon = "M4A1"};
 
-            _staff.AddChild(_pfc);
+            _staff.AddChild(_pfc1);
+            _staff.AddChild(_pfc2);
                      
             
             foreach (IPrivate soldiers in _staff)
             {
-                Console.Write(_pfc.Name);
-                Console.WriteLine("\n ID = {0}, Name = {1}, Weapon = {2}",_pfc.ID, _pfc.Name, _pfc.Weapon);
+                //Console.Write();
+                Console.WriteLine("\n ID = {0}, Name = {1}, Weapon = {2}",soldiers.ID, soldiers.Name, soldiers.Weapon);
             }
             //_staff.Operation();
         }
