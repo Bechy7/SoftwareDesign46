22b2934c6c847d3444dbd1c5c06d8c2e080de45a
FatimaAU <fatima_kodro@hotmail.com>
2018-04-11 12:03:08 +0200

    Flyttet og ændret nogle ting

--- Aflevering 3/NavyCompositePattern/NavyCompositePattern/Interfaces/IPrivate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NavyCompositePattern.Interfaces
-{
-    public class IPrivate
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Weapon { get; set; }
-    }
-}
--- Aflevering 3/NavyCompositePattern/NavyCompositePattern/Interfaces/ISpecialist.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NavyCompositePattern.Interfaces
-{
-    public class ISpecialist
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Weapon { get; set; }
-    }
-}
