c0e8ea2f180d034d9d46aec86a11bb9b65c61df3
Daniel Hansen <dph1108@gmail.com>
2018-04-11 13:27:08 +0200

    Added USCM og Operation

---
+++ Aflevering 3/NavyCompositePattern/NavyCompositePattern/Composite/Operation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NavyCompositePattern.Templates;
+
+namespace NavyCompositePattern.Composite
+{
+    //Template composite
+    public class Operation : Component, IEnumerable
+    {
+        //En operation kan indeholde 
+        //Listen skal være af den attribute der skal composite
+        private List<Component> _army = new List<Component>();
+        public string GetName { get; }
+        public string SetInfo { get; set; }
+
+        public void AddChild(Component army)
+        {
+            _army.Add(army);
+        }
+
+        public void RemoveChild(Component army)
+        {
+            _army.Remove(army);
+        }
+
+        public Component GetChild(int index)
+        {
+            return _army[index];
+        }
+        //Gør i stand til at kunne foreach det hele
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Component army in _army)
+                yield return army;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
